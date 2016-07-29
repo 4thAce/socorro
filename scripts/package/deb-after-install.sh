0d4de42ca1efde1e7d0da2faaf991fe5e105286e
Richard Magahiz <Richard.Magahiz@daqri.com>
2016-07-29 10:16:03 -0700

    Create django CACHE in deb-after-install

--- scripts/package/deb-after-install.sh
+++ scripts/package/deb-after-install.sh
@@ -3,6 +3,7 @@ INSTALL_PREFIX=/data
 
 # ensure base directories owned
 chown socorro /var/log/socorro
+mkdir $INSTALL_PREFIX/socorro/webapp-django/static/CACHE
 chown -R www-data.socorro $INSTALL_PREFIX/socorro/webapp-django/static/CACHE
 
 # crond doesn't like files with executable bits, and doesn't load
