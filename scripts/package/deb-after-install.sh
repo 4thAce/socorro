e532d6160e45e0e6b590d75c7cb29fdc77fa129f
Richard Magahiz <Richard.Magahiz@daqri.com>
2016-07-29 10:23:45 -0700

    chmod fails if the inode does not exist

--- scripts/package/deb-after-install.sh
+++ scripts/package/deb-after-install.sh
@@ -3,9 +3,9 @@ INSTALL_PREFIX=/data
 
 # ensure base directories owned
 chown socorro /var/log/socorro
-mkdir $INSTALL_PREFIX/socorro/webapp-django/static/CACHE
+mkdir $INSTALL_PREFIX/socorro/webapp-django/static/CACHE || echo "$INSTALL_PREFIX/socorro/webapp-django/static/CACHE already exists"
 chown -R www-data.socorro $INSTALL_PREFIX/socorro/webapp-django/static/CACHE
 
 # crond doesn't like files with executable bits, and doesn't load
 # them.
-chmod 644 /etc/cron.d/socorro
+touch /etc/cron.d/socorro && chmod 644 /etc/cron.d/socorro
